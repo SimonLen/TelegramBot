d43d6fac0adfed2cfca43c376458d8971f8a84b1
Semyon Sorokin <57331385+SimonLen@users.noreply.github.com>
2023-01-10 18:00:28 +0300

    TelegramBot: config added
    Telegram bot's token
    API  key of Currency Data API service
    Currencies list

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "<token>" # your telegram bot's <token> to access the HTTP API
+API_KEY = "<API key>"  # input your <API key> of Currency Data API service from https://apilayer.com/marketplace/currency_data-api
+
+currencies = {
+    "евро": "EUR",
+    "доллар": "USD",
+    "рубль": "RUB",
+    "лира": "TRY",
+    "йена": "JPY"
+}
