cd67b82a98c466f75fc97bcb3dd146b5c39e9f39
Semyon Sorokin <57331385+SimonLen@users.noreply.github.com>
2023-01-10 17:13:09 +0300

    TelegramBot: bugs fixed and handler added
    Handler with 'register_next_step_handler' added
    Bugs fixed:
    - split() method was updated for better stability
    - non-case-sensitive user input implemented

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import telebot
+from telebot import types
 from config import TOKEN, currencies
-from extensions import CurrencyConverter, APIException
+from extensions import CurrencyConverter, APIException, create_markup, amount_markup
 
 bot = telebot.TeleBot(TOKEN)
+
 
 @bot.message_handler(commands=['start', 'help'])
 def bot_info(message):
     text = 'Чтобы конвертировать валюту, отправьте сообщение в формате:\n\n' \
-           '<название валюты><в какую валюту переводим><количество переводимой валюты>\n\n' \
-           'Например: лира рубль 100\n' \
-           'Список доступных валют: /values'
+           '<название валюты><в какую валюту переводим><количество переводимой валюты>\n' \
+           'Например: лира рубль 100\n\n' \
+           'Или воспользуйтесь командой /convert\n' \
+           'Список доступных валют /values'
     bot.reply_to(message, text)
 
 
@@ -21,20 +24,54 @@ def value(message):
     bot.reply_to(message, text)
 
 
-@bot.message_handler(content_types=['text'])
-def convert(message):
+@bot.message_handler(commands=['convert'])
+def dialog_convert(message):
+    text = 'Выберете валюту для конвертации.'
+    bot.send_message(message.chat.id, text, reply_markup=create_markup())
+    bot.register_next_step_handler(message, base_handler)
+
+
+def base_handler(message: telebot.types.Message):
+    base = message.text.strip().lower()
+    text = 'В какую валюту конвертируем?'
+    bot.send_message(message.chat.id, text, reply_markup=create_markup(base))
+    bot.register_next_step_handler(message, quote_handler, base)
+
+
+def quote_handler(message: telebot.types.Message, base):
+    quote = message.text.strip()
+    text = 'Количество конвертируемой валюты:'
+    bot.send_message(message.chat.id, text, reply_markup=amount_markup)
+    bot.register_next_step_handler(message, amount_handler, base, quote)
+
+
+def amount_handler(message: telebot.types.Message, base, quote):
+    amount = message.text.strip()
     try:
-        values = message.text.split(' ')
-
-        if len(values) != 3:
-            raise APIException('Неправильный ввод. /help')
-
-        base, quote, amount = values
-        total_amount = CurrencyConverter.get_price(base, quote, amount)
+        total_amount = CurrencyConverter.get_price(base.lower(), quote.lower(), amount)
     except APIException as e:
         bot.reply_to(message, f'Ошибка пользователя. \n{e}')
     except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду. \n{e}')
+        bot.reply_to(message, f'Системная ошибка. \n{e}')
+    else:
+        text = f'{amount} {base} = {total_amount} {quote}'
+        bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(content_types=['text'])
+def message_convert(message: telebot.types.Message):
+    try:
+        values = message.text.split()
+
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров. /help')
+
+        base, quote, amount = values
+        total_amount = CurrencyConverter.get_price(base.lower(), quote.lower(), amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя. \n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Системная ошибка. \n{e}')
     else:
         text = f'{amount} {base} = {total_amount} {quote}'
         bot.send_message(message.chat.id, text)
